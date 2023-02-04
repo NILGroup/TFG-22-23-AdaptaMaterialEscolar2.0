be789071de9c68ba72e9e4b29f397b9668d81024
Alberto <rivaalberto@gmail.com>
2023-02-04 21:54:00 +0100

    Scroll en modal de ejercicio de desarrollo

--- Codigo/src/component/Modal/DesarrolloModal.jsx
+++ Codigo/src/component/Modal/DesarrolloModal.jsx
@@ -85,7 +85,6 @@ export default function DesarrolloModal({ editor, isOpen, onClose }) {
 
                         </div>
 
-
                         {<hr className={desarrolloModalStyle.modalHorizontalRule} />}
 
                         <div className={desarrolloModalStyle.vistaPrevia}>
