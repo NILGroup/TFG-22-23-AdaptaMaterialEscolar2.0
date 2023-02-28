7945abe6219afeee3671b0e6beb7a142973bcbd9
Alberto <rivaalberto@gmail.com>
2023-02-28 09:33:51 +0100

    Añadir hueco al presionar espacio

--- Codigo/src/components/Modal/MathFormula/MathFormulaModal.jsx
+++ Codigo/src/components/Modal/MathFormula/MathFormulaModal.jsx
@@ -6,6 +6,38 @@ import ModalInputNumber from "../common/ModalInputNumber";
 
 export default function DesarrolloModal({ editor, isOpen, onClose }) {
 
+	const [formula, setFormula] = useState(["4","","5","=","20"]);
+
+	const handleInputChange = (event, index) => {
+
+		console.log(event.target.value)
+		console.log(index)
+
+		let newValue = event.target.value;
+
+		setFormula(
+			formula.map((element, i)=>{
+				return i === index ? newValue : element;
+			})
+		)
+
+	};
+
+	const handleKeyDown = (event, index) => {
+		if (event.keyCode === 32) {
+			event.preventDefault();
+		  	console.log("space")
+
+			let newFormula = [...formula];
+
+			newFormula.splice(index+1,0,"")
+
+			setFormula(newFormula)
+
+		}
+	};
+
+
 	const submit = (e) => {
 		e.preventDefault();
 		
@@ -22,7 +54,39 @@ export default function DesarrolloModal({ editor, isOpen, onClose }) {
 			<div className="">
 				<form onSubmit={submit}>
 
-					<h3>Formula</h3>
+					<h3>Formula:</h3>
+
+					{/* 
+					
+					onEdit={(newValue, index) =>
+						setConcepts(
+							concepts.map((c, i) => (i === index ? newValue : c))
+						)
+					}
+					*/}
+
+					<div className="p-2 bg-gray-200 p-3">
+						
+						{
+							formula.map((elemento, i) => {
+								return (
+									<input 
+										key={i} 
+										value={elemento} 
+										onChange={e => {
+											handleInputChange(e,i);
+										}}
+										onKeyDown={e => {
+											handleKeyDown(e,i);
+										}}
+										className="w-14 p-2 border-solid border-2 border-indigo-600" 
+									/>
+								);
+							})
+						}
+
+					</div>
+
 
 					<div className="flex justify-center">
 						<button
