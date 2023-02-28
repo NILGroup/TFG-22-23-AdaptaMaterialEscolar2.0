7a589d1b7f6b1f35618e84a218b47ffd4ec2002e
AGomezSittima <agomezsittimadeveloper@gmail.com>
2023-02-28 14:53:56 +0100

    Codigo - Cambios visuales resumen

--- Codigo/src/components/Modal/Summary/SummaryModal.jsx
+++ Codigo/src/components/Modal/Summary/SummaryModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "../common/Modal";
 import ModalButton from "../common/ModalButton";
+import ModalPreview from "../common/ModalPreview";
 
 export default function SummaryModal({ editor, isOpen, onClose }) {
 	// Valores iniciales del estado
@@ -43,15 +44,23 @@ export default function SummaryModal({ editor, isOpen, onClose }) {
 					summarize(originalText);
 				}}
 			>
-				<div>
-					<h4 className="text-modal-heading">Texto original</h4>
-					<textarea name="originalText" id="originalText" className="input-textarea h-32 w-full" />
+				<div className="w-full max-w-full">
+					<div className="flex justify-between rounded-md rounded-b-none border-2 border-b-0 border-gray-300 bg-gray-200 px-4 py-2">
+						<h4 className="text-modal-heading">Texto original</h4>
+					</div>
+					<textarea
+						name="originalText"
+						id="originalText"
+						className="input-textarea h-32 w-full rounded-t-none"
+					/>
 				</div>
 				<ModalButton type="submit" className="flex items-center gap-2 self-center py-2 px-4 text-modal-base-lg">
 					Resumir
 				</ModalButton>
+
+				<hr className="my-8" />
 			</form>
-			{/* TODO: Vista previa del resumen */}
+			<ModalPreview>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sit laborum eligendi fugiat nemo quae autem pariatur architecto nostrum molestiae adipisci natus consequatur dolore ex recusandae minus, optio iste temporibus?</ModalPreview>
 		</Modal>
 	);
 }
