5a89d0755a87b3a50777816f63af3d1946bb0eef
AGomezSittima <agomezsittimadeveloper@gmail.com>
2023-03-05 18:04:31 +0100

    Codigo - Arreglos navbar

--- Codigo/src/components/Navbar/Navbar.jsx
+++ Codigo/src/components/Navbar/Navbar.jsx
@@ -10,21 +10,19 @@ import NavbarLink from "./NavbarLink";
 
 export default function Navbar() {
 	return (
-		<div className="flex w-full justify-between bg-white p-6 shadow-md">
-			<div className="max-w-[12.5rem] flex-shrink-0 hover:animate-pulse">
+		<div className="flex w-full justify-between bg-white py-4 sm:p-4 shadow-sm shadow-grey-dark">
+			<div className="max-w-[12rem] flex-shrink-0 hover:animate-pulse">
 				<Link to="/">
 					<img src={`${process.env.PUBLIC_URL}/img/Logo.png`} alt="Logo de AME2" />
 				</Link>
 			</div>
-			<div className="flex gap-2">
-				<NavbarLink to="ayuda">
-					<BiHelpCircle className="text-navbar-link-icon" />
-					<p className="text-navbar-link-text">Ayuda</p>
-				</NavbarLink>
-				<NavbarLink to="configuracion">
-					<HiOutlineCog className="text-navbar-link-icon" />
-					<p className="text-navbar-link-text">Configuración</p>
-				</NavbarLink>
+			<div className="flex items-center gap-2">
+				<NavbarLink to="ayuda" icon={<BiHelpCircle className="text-navbar-link-icon" />} text="Ayuda" />
+				<NavbarLink
+					to="configuracion"
+					icon={<HiOutlineCog className="text-navbar-link-icon" />}
+					text="Configuración"
+				/>
 			</div>
 		</div>
 	);
--- Codigo/src/components/Navbar/NavbarLink.jsx
+++ Codigo/src/components/Navbar/NavbarLink.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function NavbarLink({ className, children, ...restProps }) {
+export default function NavbarLink({ className, icon, text, ...restProps }) {
 	return (
 		<Link
-			className={`${className} flex items-center gap-2 rounded-md py-0 px-6 text-secondary transition-all duration-200 hover:bg-button hover:bg-opacity-20`}
+			className={`${className} flex items-center gap-2 rounded-full p-2 text-secondary transition-all duration-200 hover:bg-button hover:bg-opacity-20 sm:rounded-md sm:py-2 sm:px-6`}
 			{...restProps}
 		>
-			{children}
+			{icon}
+			<p className="hidden text-navbar-link-text sm:block">{text}</p>
 		</Link>
 	);
 }
