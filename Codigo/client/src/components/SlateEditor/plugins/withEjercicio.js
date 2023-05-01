ad0270d67566bdd3c26651a9d34f4f9b65a217e7
johansalgut <johansal@ucm.es>
2023-05-01 10:33:30 +0200

    Codigo - Ejercicio arreglo

--- Codigo/client/src/components/SlateEditor/plugins/withEjercicio.js
+++ Codigo/client/src/components/SlateEditor/plugins/withEjercicio.js
@@ -3,64 +3,52 @@ import { Editor, Element as SlateElement, Point, Range } from "slate";
 export const withEjercicio = (editor) => {
 	const { deleteBackward, deleteForward, insertNode } = editor;
 
-	// Evitamos que se borre la celda de la tabla cuando borremos hacia delante
+	// Evitamos que se borre el ejercicio cuando borremos hacia delante
 	// unit -> unidad a borrar
 	editor.deleteBackward = (unit) => {
 		// Obtenemos la seleccion actual del editor
 		const { selection } = editor;
 		if (selection) {
-			// Obtenemos la celda
-			const [cell] = Editor.nodes(editor, {
+			// Obtenemos el ejercicio
+			const [ejercicio] = Editor.nodes(editor, {
 				match: (n) => !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === "ejercicio",
 			});
 
-			// Obtenemos el nodo previo a la seleccion
-			const prevNodePath = Editor.before(editor, selection);
-			// Apartir del nodo previobuscamos el nodo de la tabla
-			const [ejercicioNode] = Editor.nodes(editor, {
-				at: prevNodePath,
-				match: (n) => !Editor.isEditor(n) && Element.isElement && n.type === "ejercicio",
-			});
-			// la selección está dentro de una tabla
-			if (cell) {
-				const [, cellPath] = cell;
+			// la selección está dentro de un ejercicio
+			if (ejercicio) {
+				const [, ejercicioPath] = ejercicio;
 
-				const start = Editor.start(editor, cellPath);
+				const start = Editor.start(editor, ejercicioPath);
 				if (Point.equals(selection.anchor, start)) {
 					return;
 				}
 			}
-			// la selección no está dentro de una tabla
-			if (!cell && ejercicioNode) {
+			// la selección no está dentro de un ejercicio
+			if (!ejercicio) {
 				return;
 			}
 		}
 
 		deleteBackward(unit);
 	};
-	// Evitamos que se borre la celda de la tabla cuando borremos hacia atras
+	// Evitamos que se borre el ejercicio cuando borremos hacia atras
 	editor.deleteForward = (unit) => {
 		const { selection } = editor;
 		if (selection && Range.isCollapsed(selection)) {
-			const [cell] = Editor.nodes(editor, {
+			const [ejercicio] = Editor.nodes(editor, {
 				match: (n) => !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === "ejercicio",
 			});
 
-			const prevNodePath = Editor.after(editor, selection);
-			const [tableNode] = Editor.nodes(editor, {
-				at: prevNodePath,
-				match: (n) => !Editor.isEditor(n) && Element.isElement && n.type === "ejercicio",
-			});
 
-			if (cell) {
-				const [, cellPath] = cell;
-				const end = Editor.end(editor, cellPath);
+			if (ejercicio) {
+				const [, ejercicioPath] = ejercicio;
+				const end = Editor.end(editor, ejercicioPath);
 
 				if (Point.equals(selection.anchor, end)) {
 					return;
 				}
 			}
-			if (!cell && tableNode) {
+			if (!ejercicio) {
 				return;
 			}
 		}
