99a66543dac096ca5ee6c592abbcea187278f1a4
AGomezSittima <agomezsittimadeveloper@gmail.com>
2023-05-12 23:15:23 +0200

    Codigo - Nuevo archivo

--- Codigo/client/src/components/SlateEditor/Toolbar/ToolbarGroups/ToolbarFileGroup.jsx
+++ Codigo/client/src/components/SlateEditor/Toolbar/ToolbarGroups/ToolbarFileGroup.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import exportPDF from "../../utils/exportPDF";
+import { newFile } from "../../utils/SlateUtilityFunctions";
 import ToolbarGroupButton from "./ToolbarGroupButton";
 
 export default function ToolbarFileGroup({ editor, openModal }) {
 	return (
 		<>
+			<ToolbarGroupButton onClick={() => newFile(editor)}>
+				<span>Nuevo archivo</span>
+			</ToolbarGroupButton>
 			<ToolbarGroupButton
 				onClick={() => {
 					exportPDF();
